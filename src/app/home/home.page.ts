affc7004bbb2e00a180e126cd23d7cea54ef6b1f
DaniiLlamas <daniel.llamas.patxot.alu@iesjulioverne.es>
2023-10-16 10:18:36 +0200

    calcu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  display: string;
+  operador: string;
+  primerNumero: number;
 
-  constructor() {}
+  constructor() {
+    this.display='';
+    this.operador='';
+    this.primerNumero=0;
+  }
+  agregarNumero(numero: number) {
+    this.display += numero.toString();
+  }
 
+  realizarOperacion(operador: string) {
+    if (this.display !== '') {
+      this.operador = operador;
+      this.primerNumero = parseFloat(this.display);
+      this.display = '';
+    }
+  }
+
+  borrar() {
+    this.display = '';
+    this.operador = '';
+    this.primerNumero = 0;
+  }
+  calcularResultado() {
+    if (this.operador && this.primerNumero !== null && this.display !== '') {
+      const segundoNumero = parseFloat(this.display);
+      switch (this.operador) {
+        case '+':
+          this.display = (this.primerNumero + segundoNumero).toString();
+          break;
+        case '-':
+          this.display = (this.primerNumero - segundoNumero).toString();
+          break;
+        case '*':
+          this.display = (this.primerNumero * segundoNumero).toString();
+          break;
+        case '/':
+          if (segundoNumero !== 0) {
+            this.display = (this.primerNumero / segundoNumero).toString();
+          } else {
+            this.display = 'Error';
+          }
+          break;
+      }
+      this.operador = '';
+      this.primerNumero = 0;
+    }
+  }
 }
